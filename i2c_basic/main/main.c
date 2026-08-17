78409ed499898e3e471ac136023c57259d7e5c87
rasyafwz <m.rasyafawwaz@gmail.com>
2026-08-17 00:06:01 -0700

    changed acceleration reading to interrupt based (periodic interrupt, change to WOM)

--- i2c_basic/main/main.c
+++ i2c_basic/main/main.c
@@ -16,6 +16,8 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "driver/i2c_master.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
 
 static const char *TAG = "example";
 
@@ -34,12 +36,25 @@ static const char *TAG = "example";
 
 #define ACCEL_XOUT_H                0x3B
 #define INT_ENABLE_REG              0x38
+#define I2C_MASTER_CONTROL_REG      0x36
 #define LP_ACCEL_ODR_REG            0x1E
 #define FIFO_EN_REG                 0x23
+#define USER_CTRL_REG               0x6A
+#define PWR_MGMT_2_REG              0x6C
+#define ACCEL_CONFIG1_REG           0x1C
+#define ACCEL_CONFIG2_REG           0x1D
 
 #define FIFO_CONFIG                 0x08
-#define DATA_RATE                   0x08
+#define DATA_RATE                   0x02
 #define INTERRUPT_CONFIG            0x01
+#define FIFO_RW                     0x74
+#define I2C_CONTROL                 0x40
+#define DISABLE_GYRO                0x07
+#define DISABLE_LPF                 0x08
+#define LOW_POWER_CYCLE             0x20
+
+#define GPIO_INPUT_INT              7
+#define GPIO_PIN_SEL                (1U << GPIO_INPUT_INT)
 
 //RAW_RDY_EN
 //LP_ACCEL_ODR
@@ -51,12 +66,21 @@ typedef struct {
 
 typedef struct {
     uint8_t data_rate;
+    uint8_t accel;
     uint8_t interrupt;
-    uint8_t fifo;
+    uint8_t gyro;
+    uint8_t power;
+
 
 } imu_config_t;
 
-static void i2c_write(i2c_master_dev_handle_t dev_handle, uint8_t reg_addr,)
+static QueueHandle_t gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
 
 /**
  * @brief Read a sequence of bytes from a MPU9250 sensor registers
@@ -72,7 +96,7 @@ static esp_err_t imu6500_register_read(i2c_master_dev_handle_t dev_handle, uint8
 /**
 * @brief process the acceleration buffer
 */
-static void process_buffer(uint8_t * data, uint8_t len, imu_t * imu) {
+static void process_buffer(uint8_t * data, uint8_t len, imu_data_t * imu) {
     imu->x_accel = data[0] << 8 | data[1];
     imu->y_accel = data[2] << 8 | data[3];
     imu->z_accel = data[4] << 8 | data[5];
@@ -91,22 +115,20 @@ static void imu_i2c_init_helper(i2c_master_dev_handle_t dev_handle, imu_config_t
 {
     imu6500_register_write_byte(dev_handle, LP_ACCEL_ODR_REG, imu_config->data_rate);
     imu6500_register_write_byte(dev_handle, INT_ENABLE_REG, imu_config->interrupt);
-    imu6500_register_write_byte(dev_handle, FIFO_EN_REG, imu_config->fifo);
+    imu6500_register_write_byte(dev_handle, PWR_MGMT_2_REG, imu_config->gyro);
+    imu6500_register_write_byte(dev_handle, ACCEL_CONFIG2_REG, imu_config->accel);
+    imu6500_register_write_byte(dev_handle, MPU9250_PWR_MGMT_1_REG_ADDR, imu_config->power);
 }
 
 static void imu_init(i2c_master_dev_handle_t dev_handle) {
     imu_config_t imu_config = {
         .data_rate = DATA_RATE,
         .interrupt = INTERRUPT_CONFIG,
-        .fifo = FIFO_CONFIG,
+        .gyro = DISABLE_GYRO,
+        .accel = DISABLE_LPF,
+        .power = LOW_POWER_CYCLE,
     };
-
     imu_i2c_init_helper(dev_handle, &imu_config);
-}
-
-static void read_from_fifo()
-{
-    
 }
 
 /**
@@ -134,11 +156,29 @@ static void i2c_master_init(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_
 
 void app_main(void)
 {
+    uint8_t buffer[6];
     uint8_t data[2];
-    uint8_t accel_data[6];
+
     imu_data_t imu;
+
     i2c_master_bus_handle_t bus_handle;
     i2c_master_dev_handle_t dev_handle;
+
+    uint32_t io_num;
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
+
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    io_conf.pin_bit_mask = GPIO_PIN_SEL;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    gpio_config(&io_conf);
+    gpio_set_intr_type(GPIO_INPUT_INT, GPIO_INTR_POSEDGE);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(GPIO_INPUT_INT, gpio_isr_handler, (void*) GPIO_INPUT_INT);
+
+
     i2c_master_init(&bus_handle, &dev_handle);
     ESP_LOGI(TAG, "I2C initialized successfully");
 
@@ -146,11 +186,15 @@ void app_main(void)
     ESP_ERROR_CHECK(imu6500_register_read(dev_handle, MPU9250_WHO_AM_I_REG_ADDR, data, 1));
     ESP_LOGI(TAG, "WHO_AM_I = %X", data[0]);
 
-    while (1) {
-        ESP_ERROR_CHECK(imu6500_register_read(dev_handle, ACCEL_XOUT_H, accel_data, sizeof(accel_data)));
-        process_buffer(accel_data, sizeof(accel_data), &imu);
+    imu_init(dev_handle);
 
-        printf("Acceleration (x, y, z): (%hu, %hu, %hu)\n", imu.x_accel, imu.y_accel, imu.z_accel);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    while (1) {
+        // read_from_fifo(dev_handle, buffer);
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            ESP_ERROR_CHECK(imu6500_register_read(dev_handle, ACCEL_XOUT_H, buffer, sizeof(buffer)));
+            process_buffer(buffer, sizeof(buffer), &imu);
+            printf("Acceleration (x, y, z): (%hd, %hd, %hd)\n", imu.x_accel, imu.y_accel, imu.z_accel);
+        }
+        // vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
